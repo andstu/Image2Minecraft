06aef8ac519cff08ad4f71416b2f48dfa7efdad5
andstu <ateeter@umass.edu>
2021-05-08 17:58:22 -0400

    pick blocks

--- runpyanvil.py
+++ runpyanvil.py
@@ -2,27 +2,36 @@
 import sys
 from pyanvil import World, BlockState, Material
 import numpy as np
+import pickle
 
 
-def generate_in_world(world_name, saves_folder, npy_voxels, block_type=Material.gold_block, offset_pos=(5, 5, 5)):
+def generate_in_world(world_name, saves_folder, npy_voxels, block_types=Material.gold_block, offset_pos=(5, 5, 5)):
     with World(world_name, save_location=saves_folder, debug=False) as wrld:
+        if isinstance(block_types, str):
+            with open(block_types, 'rb') as f:
+                block_types = pickle.load(f)
+        
+        
         if isinstance(npy_voxels, str) and npy_voxels[-4:] == '.npy':
             obj = np.load(npy_voxels)
         elif isinstance(npy_voxels, np.ndarray):
             obj = npy_voxels
         else:
             raise TypeError("npy_voxels is not set to a usable type.")
-        obj = obj.squeeze(0)
+#         obj = obj.squeeze(0)
         voxels = np.argwhere(obj == 1)
         voxels[:, [1, 2]] = voxels[:, [2, 1]]
-
+        
         for voxel in voxels:
-            wrld.get_block(list(voxel + offset_pos)).set_state(BlockState(block_type))
+            print('minecraft:' + block_types[tuple(voxel[[0,2, 1]])][:-4])
+            wrld.get_block(list(voxel + offset_pos)).set_state(BlockState('minecraft:' + block_types[tuple(voxel[[0,2, 1]])][:-4]))
+#             wrld.get_block(list(voxel + offset_pos)).set_state(BlockState('minecraft:gold_block'))
+
 
     print("Done!")
         
 
-generate_in_world("Flat", 'C:/Users/gsmel/AppData/Roaming/.minecraft/saves', 'toilet.npy')
+generate_in_world("Flat", r'C:\Users\atlig\AppData\Roaming\.minecraft\saves', 'Material_Extraction/cube_world.npy', block_types='Material_Extraction/voxel_to_block')
 
 
 # with World('Flat', save_location='C:/Users/gsmel/AppData/Roaming/.minecraft/saves', debug=True) as wrld:
