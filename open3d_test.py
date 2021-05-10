e53ff7a7950b1add0b3ca1354dffbb178dee6c3a
Greg Smelkov <gsmelkov@gmail.com>
2021-05-10 19:27:36 -0400

    Revert "Fixed runpyanvil"
    This reverts commit 9bba9bb19bc45aae27259236b72e36025dfae528.

--- open3d_test.py
+++
@@ -1,8 +0,0 @@
-import open3d as o3d
-from open3d import geometry
-from open3d import utility
-
-mesh = o3d.io.read_triangle_mesh("objs/cub/mesh_0.obj")
-o3d.visualization.draw_geometries([mesh])
-
-# mesh1.paint_uniform_color([1, 0.706, 0])
